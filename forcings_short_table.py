ce3214af12e4e8dd17d19476d9b7de5c7991d71a
david <d.c.hassell@reading.ac.uk>
2017-03-01 15:03:11 +0000

    forcings short table draft

---
+++ forcings_short_table.py
@@ -0,0 +1,36 @@
+{
+    "AUTHORS": [
+        "Eric Guilyardi, David Hassell"
+    ],
+    "CHANGE_HISTORY": [
+        ["0.1.0", "2017-03-01", "David Hassell, Eric Guilyardi", "Initialised"]
+    ],
+    "CONTACT": "Eric Guilyardi",
+    "CONTRIBUTORS": [],
+    "PROPERTIES": {
+        "priority1": [
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.CO2.provision",
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.CH4.provision",
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.N2O.provision",
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.tropospheric_O3.provision",
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.stratospheric_O3.provision",
+            "cmip6.model.forcings.radiative_forcing:greenhouse_gases.CFC.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.SO4.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.black_carbon.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.organic_carbon.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.nitrate.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.cloud_albedo_effect.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.cloud_lifetime_effect.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.dust.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.tropospheric_volcanic.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.stratospheric_volcanic.provision",
+            "cmip6.model.forcings.radiative_forcing:aerosols.sea_salt.provision",
+            "cmip6.model.forcings.radiative_forcing:other.land_use.provision",
+            "cmip6.model.forcings.radiative_forcing:other.solar.provision",
+        ],
+        "priority2": [],
+    }
+    "QC_STATUS": "draft"
+}
+
+
