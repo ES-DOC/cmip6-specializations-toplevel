d6b5c20daa4deab266e03ba65e5235290fe0b050
David Hassell <d.c.hassell@reading.ac.uk>
2017-11-20 09:00:17 +0000

    realm coupling 'process'

---
+++ toplevel_realm_coupling.py
@@ -0,0 +1,360 @@
+"""A model radiative forcings sepecialization.
+
+For further information goto http://wordpress.es-doc.org/cmip6-model-specializations.
+
+"""
+# --------------------------------------------------------------------
+# INTERNAL (do not change)
+# --------------------------------------------------------------------
+from collections import OrderedDict
+
+DETAILS = OrderedDict()
+ENUMERATIONS = OrderedDict()
+
+# --------------------------------------------------------------------
+# CONTACT: Set to top-level specialization co-ordinator.
+# --------------------------------------------------------------------
+CONTACT = 'David Hassell'
+
+# --------------------------------------------------------------------
+# AUTHORS: Set to top-level specialization authors (comma delimited).
+# --------------------------------------------------------------------
+AUTHORS = 'David Hassell, Eric Guilyardi'
+
+# --------------------------------------------------------------------
+# QUALITY CONTROL STATUS: Set to 'draft' or 'complete'
+# --------------------------------------------------------------------
+QC_STATUS = 'draft'
+
+# --------------------------------------------------------------------
+# DESCRIPTION: Short description of the specialization.
+# --------------------------------------------------------------------
+DESCRIPTION = 'Coupling between model realms'
+
+_fields = 'Enter the coupled fields, their coupling frequency and any coupling details as a comma seperated list. E.g. temperature, daily, area-weighted interpolotaion'
+
+# --------------------------------------------------------------------
+# Coupling from OCEAN to other realms
+# --------------------------------------------------------------------
+_from = 'ocean'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'ocean'
+_to   = 'seaice'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'ocean'
+_to   = 'ocean_biogeochemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from ATMOSPHERE to other realms
+# --------------------------------------------------------------------
+_from = 'atmosphere'
+_to   = 'ocean'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmosphere'
+_to   = 'seaice'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmosphere'
+_to   = 'land'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmosphere'
+_to   = 'aerosols'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmosphere'
+_to   = 'amospheric_chemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+
+_from = 'atmosphere'
+_to   = 'ocean_biogeochemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from SEAICE to other realms
+# --------------------------------------------------------------------
+_from = 'seaice'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'seaice'
+_to   = 'ocean'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'seaice'
+_to   = 'ocean_biogeochemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from LAND to other realms
+# --------------------------------------------------------------------
+_from = 'land'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'land'
+_to   = 'ocean_biogeochemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'land'
+_to   = 'atmospheric_chemistry'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from AEROSOLS to other realms
+# --------------------------------------------------------------------
+_from = 'aerosols'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from ATMOSPHERIC CHEMISTRY to other realms
+# --------------------------------------------------------------------
+_from = 'atmospheric_chemistry'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmospheric_chemistry'
+_to   = 'land'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmospheric_chemistry'
+_to   = 'land_ice'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'atmospheric_chemistry'
+_to   = 'sea_ice'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from OCEAN BIOGEOCHEMISTRY to other realms
+# --------------------------------------------------------------------
+_from = 'ocean_biogeochemistry'
+_to   = 'ocean'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'ocean_biogeochemistry'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'ocean_biogeochemistry'
+_to   = 'seaice'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+# --------------------------------------------------------------------
+# Coupling from LAND ICE to other realms
+# --------------------------------------------------------------------
+_from = 'land_ice'
+_to   = 'ocean'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
+_from = 'land_ice'
+_to   = 'atmosphere'
+DETAILS[_from+'_to_'+_to] = {
+    'description': _from.replace('_', ' ').capitalize()+' to '+_to.replace('_', ' ')+' interface couplings',
+    'properties':[
+        ('overview', 'str', '1.1',
+            'Overview of '+_from.replace('_', ' ')+' to '+_to.replace('_', ' ')+' coupling interface'),
+        ('fields', 'str', '0.N',
+             _fields),
+        ]
+    }
+
